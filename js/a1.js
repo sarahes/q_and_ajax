f748b4d18074c6d1002c0026081dcf9ba31cc2f8
Sarah Sheehan <ssheehan131@gmail.com>
2012-07-10 21:08:59 -0400

    convert tabs to spaces so code doesn't look wonky on github

--- js/a1.js
+++ js/a1.js
@@ -1,131 +1,131 @@
 function sendRequest(answers, whichAnswer) {
-	var xhr = new XMLHttpRequest();
-	if (xhr) 
-	{
-		xhr.onreadystatechange = function() 
-		{ 
-			displayResponse(xhr, whichAnswer);
-		}
-		xhr.open("GET", answers, true);
-		xhr.send(null);
-	}
+    var xhr = new XMLHttpRequest();
+    if (xhr) 
+    {
+        xhr.onreadystatechange = function() 
+        { 
+            displayResponse(xhr, whichAnswer);
+        }
+        xhr.open("GET", answers, true);
+        xhr.send(null);
+    }
 }
 
 function displayResponse(xhr, whichAnswer) 
 {
-	if (xhr.readyState == 4) 
-	{
-		if (xhr.status == 200 || xhr.status == 304) 
-		{			
-			if (whichAnswer == 'answer1')
-			{
+    if (xhr.readyState == 4) 
+    {
+        if (xhr.status == 200 || xhr.status == 304) 
+        {			
+            if (whichAnswer == 'answer1')
+            {
                 var answerDiv1 = document.getElementById('answer1');	
                 answerDiv1.innerHTML = "<h3>Answers: </h3>";
                 answerDiv1.innerHTML += "<ol>";
-				lines = xhr.responseText.split("\n");
-				for(var i = 0; i <= lines.length; i++)
-				{
-					if(lines[i].indexOf("q1*") != -1)
-					{	
+                lines = xhr.responseText.split("\n");
+                for(var i = 0; i <= lines.length; i++)
+                {
+                    if(lines[i].indexOf("q1*") != -1)
+                    {	
                         answer = lines[i].substring(lines[i].indexOf("*") + 1);
-						answerDiv1.innerHTML += "<li>" + answer + "</li>";
-						
-					}
-				}	
+                        answerDiv1.innerHTML += "<li>" + answer + "</li>";
+                        
+                    }
+                }	
                 answerDiv1.innerHTML += "</ol>";
-			}
-			if (whichAnswer == 'answer2')
-			{
-				var answerDiv2 = document.getElementById('answer2');	
+            }
+            if (whichAnswer == 'answer2')
+            {
+                var answerDiv2 = document.getElementById('answer2');	
                 answerDiv2.innerHTML = "<h3>Answers: </h3>";
                 answerDiv2.innerHTML += "<ol>";
-				lines = xhr.responseText.split("\n");
-				for(var i = 0; i <= lines.length; i++)
-				{
-					if(lines[i].indexOf("q2*") != -1)
-					{	
+                lines = xhr.responseText.split("\n");
+                for(var i = 0; i <= lines.length; i++)
+                {
+                    if(lines[i].indexOf("q2*") != -1)
+                    {	
                         answer = lines[i].substring(lines[i].indexOf("*") + 1);
-						answerDiv2.innerHTML += "<li>" + answer + "</li>";
-						
-					}
-				}	
+                        answerDiv2.innerHTML += "<li>" + answer + "</li>";
+                        
+                    }
+                }	
                 answerDiv2.innerHTML += "</ol>";
-			}
-			if (whichAnswer == 'answer3')
-			{
-				var answerDiv3 = document.getElementById('answer3');	
+            }
+            if (whichAnswer == 'answer3')
+            {
+                var answerDiv3 = document.getElementById('answer3');	
                 answerDiv3.innerHTML = "<h3>Answers: </h3>";
                 answerDiv3.innerHTML += "<ol>";
-				lines = xhr.responseText.split("\n");
-				for(var i = 0; i <= lines.length; i++)
-				{
-					if(lines[i].indexOf("q3*") != -1)
-					{	
+                lines = xhr.responseText.split("\n");
+                for(var i = 0; i <= lines.length; i++)
+                {
+                    if(lines[i].indexOf("q3*") != -1)
+                    {	
                         answer = lines[i].substring(lines[i].indexOf("*") + 1);
-						answerDiv3.innerHTML += "<li>" + answer + "</li>";
-						
-					}
-				}	
+                        answerDiv3.innerHTML += "<li>" + answer + "</li>";
+                        
+                    }
+                }	
                 answerDiv3.innerHTML += "</ol>";
-			}
-			if (whichAnswer == 'answer4')
-			{
-				var answerDiv4 = document.getElementById('answer4');	
+            }
+            if (whichAnswer == 'answer4')
+            {
+                var answerDiv4 = document.getElementById('answer4');	
                 answerDiv4.innerHTML = "<h3>Answers: </h3>";
                 answerDiv4.innerHTML += "<ol>";
-				lines = xhr.responseText.split("\n");
-				for(var i = 0; i <= lines.length; i++)
-				{
-					if(lines[i].indexOf("q4*") != -1)
-					{	
+                lines = xhr.responseText.split("\n");
+                for(var i = 0; i <= lines.length; i++)
+                {
+                    if(lines[i].indexOf("q4*") != -1)
+                    {	
                         answer = lines[i].substring(lines[i].indexOf("*") + 1);
-						answerDiv4.innerHTML += "<li>" + answer + "</li>";
-						
-					}
-				}	
+                        answerDiv4.innerHTML += "<li>" + answer + "</li>";
+                        
+                    }
+                }	
                 answerDiv4.innerHTML += "</ol>";
-			}
-			if (whichAnswer == 'answer5')
-			{
-				var answerDiv5 = document.getElementById('answer5');	
+            }
+            if (whichAnswer == 'answer5')
+            {
+                var answerDiv5 = document.getElementById('answer5');	
                 answerDiv5.innerHTML = "<h3>Answers: </h3>";
                 answerDiv5.innerHTML += "<ol>";
-				lines = xhr.responseText.split("\n");
-				for(var i = 0; i <= lines.length; i++)
-				{
-					if(lines[i].indexOf("q5*") != -1)
-					{	
+                lines = xhr.responseText.split("\n");
+                for(var i = 0; i <= lines.length; i++)
+                {
+                    if(lines[i].indexOf("q5*") != -1)
+                    {	
                         answer = lines[i].substring(lines[i].indexOf("*") + 1);
-						answerDiv5.innerHTML += "<li>" + answer + "</li>";
-						
-					}
-				}	
+                        answerDiv5.innerHTML += "<li>" + answer + "</li>";
+                        
+                    }
+                }	
                 answerDiv1.innerHTML += "</ol>";
-			}
-			if (whichAnswer == 'answer6')
-			{
-				var answerDiv6 = document.getElementById('answer6');	
+            }
+            if (whichAnswer == 'answer6')
+            {
+                var answerDiv6 = document.getElementById('answer6');	
                 answerDiv6.innerHTML = "<h3>Answers: </h3>";
                 answerDiv6.innerHTML += "<ol>";
-				lines = xhr.responseText.split("\n");
-				for(var i = 0; i <= lines.length; i++)
-				{
-					if(lines[i].indexOf("q6*") != -1)
-					{	
+                lines = xhr.responseText.split("\n");
+                for(var i = 0; i <= lines.length; i++)
+                {
+                    if(lines[i].indexOf("q6*") != -1)
+                    {	
                         answer = lines[i].substring(lines[i].indexOf("*") + 1);
-						answerDiv6.innerHTML += "<li>" + answer + "</li>";
-						
-					}
-				}	
+                        answerDiv6.innerHTML += "<li>" + answer + "</li>";
+                        
+                    }
+                }	
                 answerDiv6.innerHTML += "</ol>";
-			}
-		
-		}
-		else if (xhr.status == 404) 
-		{
-			
-		}
-	}
+            }
+        
+        }
+        else if (xhr.status == 404) 
+        {
+            
+        }
+    }
 }			
 
 function show() 
@@ -172,59 +172,59 @@ function show()
 window.onload = function() 
 {
     // add event listeners to answer buttons + send ajax request
-	var answer1 = document.getElementById('q1');
-	answer1.addEventListener('click', function(e) 
-	{
-		e.preventDefault();
+    var answer1 = document.getElementById('q1');
+    answer1.addEventListener('click', function(e) 
+    {
+        e.preventDefault();
         document.getElementById("answer1").setAttribute("class", "answer show");
-		var whichAnswer = 'answer1';
-		sendRequest(this.href, whichAnswer);
-	}, false);
+        var whichAnswer = 'answer1';
+        sendRequest(this.href, whichAnswer);
+    }, false);
 
-	var answer2 = document.getElementById('q2');
-	answer2.addEventListener('click', function(e) 
-	{
-		e.preventDefault();
+    var answer2 = document.getElementById('q2');
+    answer2.addEventListener('click', function(e) 
+    {
+        e.preventDefault();
         document.getElementById("answer2").setAttribute("class", "answer show");
-		var whichAnswer = 'answer2';
-		sendRequest(this.href, whichAnswer);
-	}, false);
-	
-	var answer3 = document.getElementById('q3');
-	answer3.addEventListener('click', function(e) 
-	{
-		e.preventDefault();
+        var whichAnswer = 'answer2';
+        sendRequest(this.href, whichAnswer);
+    }, false);
+    
+    var answer3 = document.getElementById('q3');
+    answer3.addEventListener('click', function(e) 
+    {
+        e.preventDefault();
         document.getElementById("answer3").setAttribute("class", "answer show");
-		var whichAnswer = 'answer3';
-		sendRequest(this.href, whichAnswer);
-	}, false);
-	
-	var answer4 = document.getElementById('q4');
-	answer4.addEventListener('click', function(e) 
-	{
-		e.preventDefault();
+        var whichAnswer = 'answer3';
+        sendRequest(this.href, whichAnswer);
+    }, false);
+    
+    var answer4 = document.getElementById('q4');
+    answer4.addEventListener('click', function(e) 
+    {
+        e.preventDefault();
         document.getElementById("answer4").setAttribute("class", "answer show");
-		var whichAnswer = 'answer4';
-		sendRequest(this.href, whichAnswer);
-	}, false);
-	
-	var answer5 = document.getElementById('q5');
-	answer5.addEventListener('click', function(e) 
-	{
-		e.preventDefault();
+        var whichAnswer = 'answer4';
+        sendRequest(this.href, whichAnswer);
+    }, false);
+    
+    var answer5 = document.getElementById('q5');
+    answer5.addEventListener('click', function(e) 
+    {
+        e.preventDefault();
         document.getElementById("answer5").setAttribute("class", "answer show");
-		var whichAnswer = 'answer5';
-		sendRequest(this.href, whichAnswer);
-	}, false);	
-	
-	var answer6 = document.getElementById('q6');
-	answer6.addEventListener('click', function(e) 
-	{
-		e.preventDefault();
+        var whichAnswer = 'answer5';
+        sendRequest(this.href, whichAnswer);
+    }, false);	
+    
+    var answer6 = document.getElementById('q6');
+    answer6.addEventListener('click', function(e) 
+    {
+        e.preventDefault();
         document.getElementById("answer6").setAttribute("class", "answer show");
-		var whichAnswer = 'answer6';
-		sendRequest(this.href, whichAnswer);
-	}, false);
+        var whichAnswer = 'answer6';
+        sendRequest(this.href, whichAnswer);
+    }, false);
     
     //add event listeners for tag list to hide show certain questions
     document.getElementById('all').addEventListener('click',show,false);
@@ -233,5 +233,5 @@ window.onload = function()
     document.getElementById('js').addEventListener('click',show,false);
     document.getElementById('ajax').addEventListener('click',show,false);   
 }
-	
+    
  
